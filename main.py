975571d4d8cb4fa088b2d3a7e203b73a39d5bcc3
Пан Микола <nickkolomietsdooor@gmail.com>
2022-11-30 22:37:35 +0200

    adding movment

--- main.py
+++ main.py
@@ -38,8 +38,12 @@ class Game:
         self.functions = [self.clear, self.edges.clear, self.clear_segments, self.add_points, self.voronoi, self.delone,
                           self.djarvis, self.grehem, self.recursive, self.movement]
         self.is_moving = False
-        self.speed = 2
+        self.speed = 4
         self.angels = []
+
+        self.distance = 3
+        self.rad_edges = 8
+        self.menu_width = 150
 
     def upload_settings(self):
         with open('settings.json') as file:
@@ -58,16 +62,23 @@ class Game:
         cords = np.array(cords)
 
         for edge1 in self.edges:
-            if edge1.distance(cords) <= 18:
+            if edge1.distance(cords) < 2 * self.rad_edges:
                 return
 
-        if cords[0] < 15 or cords[0] > 1110 or cords[1] < 15 or cords[1] > 700:
+        if cords[0] < self.rad_edges + self.distance or \
+                cords[0] > self.settings["screen"]["width"] - self.menu_width - self.distance - self.rad_edges or \
+                cords[1] < self.rad_edges + self.distance or \
+                cords[1] > self.settings["screen"]["heights"] - self.rad_edges - + self.distance:
             return
         self.edges.append(Edge(cords))
+
+        if self.is_moving:
+            self.angels.append(random.random() * 2 * scipy.constants.pi)
 
     def clear(self):
         self.edges.clear()
         self.clear_segments()
+        self.angels.clear()
 
     def clear_segments(self):
         self.segments_delone.clear()
@@ -75,8 +86,11 @@ class Game:
         self.segments_lin.clear()
 
     def add_points(self):
-        for i in range(10):
-            self.new_edge((int(1110 * random.random()), int(700 * random.random())))
+        for i in range(100):
+            self.new_edge((
+                int((self.settings["screen"]["width"] - self.menu_width -
+                     self.distance - self.rad_edges) * random.random()),
+                int((self.settings["screen"]["heights"] - self.rad_edges - + self.distance) * random.random())))
 
     def voronoi(self):
 
@@ -148,15 +162,15 @@ class Game:
 
         for edge in self.edges:
             pygame.draw.circle(surface=self.screen, color=colors[self.settings["edge"]["color"]],
-                               center=(int(edge.cords[0]), int(edge.cords[1])), radius=8, width=0)
+                               center=(int(edge.cords[0]), int(edge.cords[1])), radius=self.rad_edges, width=0)
 
     def update_frames(self):
 
-        pygame.draw.rect(self.screen, colors['white'], (self.settings["screen"]["width"] - 150, 3,
+        pygame.draw.rect(self.screen, colors['white'], (self.settings["screen"]["width"] - self.menu_width, 3,
                                                         self.settings["screen"]["width"],
                                                         self.settings["screen"]["heights"]))
 
-        pygame.draw.rect(self.screen, colors['black'], (self.settings["screen"]["width"] - 150, 3, 148,
+        pygame.draw.rect(self.screen, colors['black'], (self.settings["screen"]["width"] - self.menu_width, 3, 148,
                                                         self.settings["screen"]["heights"] - 3), width=6)
 
         pygame.draw.rect(self.screen, colors['black'], (3, 3, self.settings["screen"]["width"] - 6,
@@ -165,7 +179,7 @@ class Game:
     def update_buttons(self):
         for i, button in enumerate(self.buttons):
             self.screen.blit(button, (self.settings["screen"]["width"] - 140, 40 * i + 6))
-            pygame.draw.rect(self.screen, colors['black'], (self.settings["screen"]["width"] - 150, 40 * i,
+            pygame.draw.rect(self.screen, colors['black'], (self.settings["screen"]["width"] - self.menu_width, 40 * i,
                                                             self.settings["screen"]["width"] - 6, 40), width=2)
 
     def update(self):
@@ -193,17 +207,44 @@ class Game:
         else:
             self.move()
 
-    def moving(self):
-        for i in range(len(self.edges)):
-            if self.edges[i].cords[0] < 15 or self.edges[i].cords[0] > 1110:
-                self.angels[i] += scipy.constants.pi
-                self.edges[i].cords[0] -= 4 * (self.edges[i].cords[0] - 15) / abs(self.edges[i].cords[0] - 15)
-            if self.edges[i].cords[1] < 15 or self.edges[i].cords[1] > 700:
-                self.angels[i] = scipy.constants.pi - self.angels[i]
-                self.edges[i].cords[1] -= 4 * (self.edges[i].cords[1] - 15) / abs(self.edges[i].cords[1] - 15)
+    def collisions_border(self, i, v_x, v_y):
+        if self.edges[i][0] + v_x < self.rad_edges + self.distance:
+            self.angels[i] = 3 * scipy.constants.pi - self.angels[i]
+        elif self.edges[i][0] + v_x > self.settings["screen"]["width"] - \
+                self.menu_width - self.distance - self.rad_edges:
+            self.angels[i] = scipy.constants.pi - self.angels[i]
 
-            v_y = math.cos(self.angels[i]) * self.speed
-            v_x = math.sin(self.angels[i]) * self.speed
+        if self.edges[i][1] + v_y < self.rad_edges + self.distance:
+            self.angels[i] = 4 * scipy.constants.pi - self.angels[i]
+        elif self.edges[i][1] + v_y > self.settings["screen"]["heights"] - self.rad_edges - + self.distance:
+            self.angels[i] = 2 * scipy.constants.pi - self.angels[i]
+
+    def collisions_edges(self, i):
+        for edge1 in self.edges:
+            if 0.1 < edge1.distance(self.edges[i].cords) <= 2 * self.rad_edges:
+                alfa = ((edge1[0] - self.edges[i][0]) /
+                        ((edge1[0] - self.edges[i][0]) ** 2 + (edge1[1] - self.edges[i][1]) ** 2) ** (
+                                1 / 2))
+                alfa = alfa if (edge1[0] - self.edges[i][0]) * (edge1[1] - self.edges[i][1]) > 0 else -alfa
+                alfa = math.asin(alfa)
+                alfa += scipy.constants.pi / 2
+                self.angels[i] = 2 * alfa - self.angels[i]
+
+    def collisions(self):
+        for i in range(len(self.edges)):
+            v_x = math.cos(self.angels[i]) * self.speed
+            v_y = math.sin(self.angels[i]) * self.speed
+            self.collisions_border(i, v_x, v_y)
+            #self.collisions_edges(i)
+
+    def moving(self):
+
+        self.collisions()
+
+        for i in range(len(self.edges)):
+            v_x = math.cos(self.angels[i]) * self.speed
+            v_y = math.sin(self.angels[i]) * self.speed
+
             self.edges[i].cords[0] += v_x
             self.edges[i].cords[1] += v_y
 
@@ -219,7 +260,7 @@ class Game:
                     exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if pygame.mouse.get_pressed()[0]:
-                        if event.pos[0] < self.settings["screen"]["width"] - 150:
+                        if event.pos[0] < self.settings["screen"]["width"] - self.menu_width:
                             self.new_edge(event.pos)
                         else:
                             self.functions[event.pos[1] // 40]()
